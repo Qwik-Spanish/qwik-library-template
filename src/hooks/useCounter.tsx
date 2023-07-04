e0e15bcacab5e31f3f35e2456899cf2cf0691745
Anartz Mugika Ledo <mugan86@gmail.com>
2023-07-04 20:18:30 +0200

    feat: template first version ok with basic elements

--- src/hooks/useCounter.tsx
+++ src/hooks/useCounter.tsx
@@ -1,19 +1,17 @@
-import { useSignal, $ } from '@builder.io/qwik';
+import { useSignal, $ } from "@builder.io/qwik";
 
 const useCounter = (initialValue = 0) => {
-  // 1
   const counter = useSignal(initialValue);
-
-  // 2
   const increment = $(() => counter.value++);
+  const decrement = $(() => counter.value--);
   const reset = $(() => (counter.value = 0));
 
-  // 3
   return {
     counter,
     increment,
+    decrement,
     reset,
   };
 };
 
-export { useCounter} ;
+export { useCounter };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-export { Logo } from './components/logo/logo';
-export { Counter } from './components/counter/counter';
+export { Logo } from './components/logo';
+export { Counter } from './components/counter';
 export { useCounter } from './hooks/useCounter';
-export type { LogoProps } from './components/logo/logo';
-export { DateInput } from './components/form/date';
+export type { LogoProps } from './components/logo';
